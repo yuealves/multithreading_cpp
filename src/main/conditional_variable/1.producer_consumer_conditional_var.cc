bc3e1aa872e64f7a505139342830b3f952a892d6
linwei <linweilifudan@gmail.com>
2021-03-09 00:55:02 +0800

    add some Chinese comments

--- src/main/conditional_variable/1.producer_consumer_conditional_var.cc
+++ src/main/conditional_variable/1.producer_consumer_conditional_var.cc
@@ -31,9 +31,18 @@ void Consumer() {
   for (int i = 0; i < 100; i++) {
     std::unique_lock<std::mutex> ul(g_mutex);
 
-    // if blocked, ul.unlock() is automatically called.
-    // if unblocked, ul.lock() is automatically called.
+    // g_cv.wait会阻塞当前线程，并调用ul.unlock()释放锁使得其它被阻塞在锁竞争上的线程得以继续执行
+    // g_cv.wait阻塞当前线程会一直持续到其它线程notify_*唤醒该线程，唤醒时会自动调用ul.lock()，
+    // 使得其他使用ul锁的线程的对应critical section阻塞
+
+    // 由于线程有可能被各种操作系统意外的情况唤醒，故而在被唤醒时还需要检查flag是否ready，故而一个
+    // 保险的写法是：
+    // while(!g_ready){
+    //   g_cv.wait(ul);
+    // }
+    // 下述一行与上面的while循环是等效的
     g_cv.wait(ul, []() { return g_ready; });
+
     // Sample data
     data = g_data;
     std::cout << "data: " << data << std::endl;
